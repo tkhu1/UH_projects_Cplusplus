75bf38ee3a0d6948cacde302ac19fdb6573c54ab
Tyler Hu <46427408+tkhu1@users.noreply.github.com>
2019-05-14 17:42:40 -0500

    Update main.cpp

--- intro_level/spring_2019/homework/03_caesar_cipher/main.cpp
+++ intro_level/spring_2019/homework/03_caesar_cipher/main.cpp
@@ -11,21 +11,21 @@ string encryptor(string user_word, unsigned long long int shift)
     //loops through every letter 
     for (int letter = 0; letter < user_word.length(); letter++) 
     { 
-       //encrypts uppercase letters 
-       if (isupper(user_word[letter])) 
-       {
+        //encrypts uppercase letters 
+        if (isupper(user_word[letter])) 
+        {
           result += (((user_word[letter] - 'A') + shift) % 26 + 'A'); 
-       }
-       //encrypts lowercase letters 
-       else if (islower(user_word[letter]))
-       {
-          result += (((user_word[letter] - 'a') + shift) % 26 + 'a'); 
-       }
-       //does not encrypt non-alpha chars
-       else
-       {
-          result += user_word[letter];
-       }
+        }
+        //encrypts lowercase letters 
+        else if (islower(user_word[letter]))
+        {
+           result += (((user_word[letter] - 'a') + shift) % 26 + 'a'); 
+        }
+        //does not encrypt non-alpha chars
+        else
+        {
+           result += user_word[letter];
+        }
     }
     //returns the resulting string 
     return result; 
@@ -40,23 +40,18 @@ int main()
     bool cont2 = true; //checks if user wishes to quit program 
 
    //implements a loop so that it will continue asking until the user provides a positive integer
-   while(cont1)
+   while(cont1) 
    {  
-         cout <<"How many words are in your message? \n";
-         cout <<"Enter value: ";
+       cout <<"How many words are in your message? \n";
+       cout <<"Enter value: ";
          
-         // get user input integer here
-         cin >> num_words;
+       // get user input integer here
+       cin >> num_words;
       
-      if (num_words <= 0) 
-      {
-         cout <<"\nInvalid value. Please Re-enter a number of positive value\n";
-      }
-      else //leaves loop
-      {
+       if (num_words <= 0) 
+           cout <<"\nInvalid value. Please Re-enter a number of positive value\n";
+       else //leaves loop
          cont1 = false;
-      }
-         
    }
 
    //declares a dynamic array of string type with the specified number of elements
@@ -64,7 +59,8 @@ int main()
    encrypt_arr = new string[num_words];
    string* enc_ptr = encrypt_arr; //pointer to array
     
-   while(cont2) {
+   while(cont2) 
+   {
        cout <<"\nSelect one of these options: (1) Get Message (2) Encrypt (3) Print (4) Quit";
        cout <<"\nSelection: ";
        
@@ -74,25 +70,22 @@ int main()
        switch(user_choice)
        {
            case 1: //Get Message
-               
                //gets the specified number of words from the user
                cout << "\nEnter Message to Encrypt: ";
  
                //loop outputs from string buffer into array
                for (int i = 0; i < num_words; i++)
                {
-                  cin >> encrypt_arr[i];
-                  //cout << encrypt_arr[i]; //DEBUG
+                   cin >> encrypt_arr[i];
+                   //cout << encrypt_arr[i]; //DEBUG
                }
 
                //gets shifts from the user
                cout << "\nEnter number of letters to shift: ";
                cin >> user_shift;
-               
                break;
            
            case 2: //Encrypt
-
                enc_ptr = encrypt_arr; //resets pointer position
                //loop calls encryptor function with pointer parameter
                for (int j = 0; j < num_words; j++)
@@ -102,11 +95,9 @@ int main()
                   
                   //cout << encrypt_arr[j]; //DEBUG
                }                
-               
                break;
 
            case 3: //Print
-           
                enc_ptr = encrypt_arr; //resets pointer position
                
                cout << "\n" << "The message is: "; //for formatting
@@ -127,12 +118,11 @@ int main()
    
            default:
                break;
-      }
+        }
     }
     
     //releases the memory allocated above
     delete [] encrypt_arr;
     
     return 0;
-
 }
