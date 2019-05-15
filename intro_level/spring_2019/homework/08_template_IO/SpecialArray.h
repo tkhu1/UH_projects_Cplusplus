17de3f3d5f78d2e5ef91c6829eb6daad16608d93
Tyler Hu <46427408+tkhu1@users.noreply.github.com>
2019-05-14 19:30:17 -0500

    Add files via upload

---
+++ intro_level/spring_2019/homework/08_template_IO/SpecialArray.h
@@ -0,0 +1,169 @@
+//headers here
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+#ifndef SpecialArray_H
+#define SpecialArray_H
+
+template<typename TheType>
+class SpecialArray {
+	public:
+		//default
+		SpecialArray(); 
+
+		//constructor
+		SpecialArray(int _rows, int _cols): numRows(_rows), numCols(_cols) {
+			arr = new TheType* [numRows];
+			for (int thisRow = 0; thisRow < numRows; thisRow++) {
+				arr[thisRow] = new TheType[numCols];
+			}
+		}
+
+		//RO3//////////////////////////////////////////////////////////////////////////////
+		//copy constructor
+		SpecialArray(const SpecialArray &other) {
+			arr = new TheType* [other.numRows];
+			for (int thisRow = 0; thisRow < other.numRows; thisRow++) {
+				arr[thisRow] = new TheType[other.numCols];
+			}
+			// loops and copies 'other' array into 'arr' array
+			for (int row = 0; row < other.numRows; row++) {
+				for (int col = 0; col < other.numCols; col++) {
+					arr[row][col] = other.arr[row][col];
+				}
+			}
+    	// copies members   
+	    numRows = other.numRows;
+	  	numCols = other.numCols;
+		}
+
+		//copy assignment operator
+		SpecialArray& operator=(const SpecialArray& rhs) {
+			// check for self assignment 
+			if (this != &rhs) { 
+				delete [] arr;
+				
+				arr = new TheType* [rhs.numRows];
+				for (int thisRow = 0; thisRow < rhs.numRows; thisRow++) {
+					arr[thisRow] = new TheType[rhs.numCols];
+				}
+				// loops and copies 'other' array into 'arr' array
+				for (int row = 0; row < rhs.numRows; row++) {
+					for (int col = 0; col < rhs.numCols; col++) {
+						arr[row][col] = rhs.arr[row][col];
+					}
+				}
+				// copies members   
+				numRows = rhs.numRows;
+				numCols = rhs.numCols;
+			}
+			return *this;
+		}
+
+		//destructor
+		~SpecialArray() { 
+			delete [] arr;
+			arr = NULL;
+		}
+		//FUNCTIONS////////////////////////////////////////////////////////////////////////
+		//file reader
+		void readFile(ifstream &input) {
+			TheType inElement;
+
+			for (int row = 0; row < numRows; row++) {
+					for (int col = 0; col < numCols; col++) {
+						input >> inElement;
+						arr[row][col] = inElement;
+						//cout << arr[row][col]; DEBUG
+					}
+				}
+		}
+
+		//returns max 
+		TheType max() {
+			TheType maxResult = arr[0][0];
+
+			for (int row = 0; row < numRows; row++) {
+					for (int col = 0; col < numCols; col++) {
+						if (arr[row][col] > maxResult)
+							maxResult = arr[row][col];
+					}
+				}
+			return maxResult;
+		}
+
+		//returns min 
+		TheType min() {
+			TheType minResult = arr[0][0];
+
+			for (int row = 0; row < numRows; row++) {
+					for (int col = 0; col < numCols; col++) {
+						if (arr[row][col] < minResult)
+							minResult = arr[row][col];
+					}
+				}
+			return minResult;
+		}
+
+		//sorts array
+		void sort() {
+			TheType temp;
+      
+			for (int row = 0; row < numRows; row++) {
+				//bubble sorter
+				for (int col = 1; col < numCols; col++) {
+					for (int element = 0; element < numCols-1; element++) {
+						if (arr[row][element] > arr[row][element+1]) {
+							temp = arr[row][element];
+							arr[row][element] = arr[row][element+1];
+							arr[row][element+1] = temp;
+						}
+					}
+				}
+			}
+		}
+
+		//prints to display
+		void print() {
+			for (int row = 0; row < numRows; row++) {
+					for (int col = 0; col < numCols; col++) {
+						cout << arr[row][col] << " ";
+					}
+					cout << endl;
+				}
+		}
+
+		//writes to file
+		void saveToFile() {
+			string outFilename = "output.txt";
+			TheType output;
+			//output ops
+			ofstream outFile;
+			outFile.open(outFilename);
+
+			if (!outFile.is_open()) {
+      cout << "Could not open file." << endl;
+   		}
+	
+			for (int row = 0; row < numRows; row++) {
+				for (int col = 0; col < numCols; col++) {
+					output = arr[row][col];
+					outFile << output << " ";
+				}
+				outFile << endl;
+			}
+			outFile.close();
+		}
+
+	private:
+		int numRows;
+		int numCols;
+		TheType **arr; //pointer for a 2D dynamic array.
+
+};
+
+#endif
+
---
+++ intro_level/spring_2019/homework/08_template_IO/main.cpp
@@ -0,0 +1,86 @@
+#include "SpecialArray.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int measureElementsPerLine(ifstream& inFile) {
+	string temp;
+	int counter = 1;
+	getline(inFile, temp); //gets first line of file into temp string
+	for (int i = 0; i < temp.size(); i++) {
+		if (temp[i] == ' ') {
+			counter++;
+		}
+  }   
+	//cout << counter << endl; //DEBUG
+	return counter;
+}
+
+int measureLines(ifstream& inFile) {
+  string temp;
+	int counter = 0;
+	while (getline(inFile, temp)) {
+	 	counter++;
+	 }
+	//cout << counter << endl; //DEBUG
+	return counter;
+}
+
+int main()
+{
+	int numOfLines, numOfElements;
+	string fileName, dataType;
+	cin >> fileName >> dataType;
+  ifstream inFile(fileName);
+
+  // Add try statement below
+  try {
+		//checks if file exists
+		if (!inFile.is_open()) {
+			throw runtime_error("File does not exist.");
+		}
+		//checks if file is empty
+		inFile.seekg (0, inFile.end);
+		if (inFile.tellg() == 0) {
+			throw runtime_error("File is empty.");
+		}
+		inFile.clear();
+		inFile.seekg(0, inFile.beg);
+	   
+		numOfElements = measureElementsPerLine(inFile);
+		inFile.close();
+		inFile.open(fileName);
+		numOfLines = measureLines(inFile);
+		inFile.close();
+
+		if (dataType == "int"){
+				SpecialArray<int> specialArray(numOfLines, numOfElements);
+				inFile.open(fileName);
+			specialArray.readFile(inFile);
+			inFile.close();
+			specialArray.print();
+			specialArray.sort();
+			cout << "\nSorted outputs: \n";
+			specialArray.print();
+		}
+		else if (dataType == "string"){
+				SpecialArray<string> specialArray(numOfLines, numOfElements);
+				inFile.open(fileName);
+			specialArray.readFile(inFile);
+			inFile.close();   
+			specialArray.print();
+			specialArray.sort();
+			cout << "\nSorted outputs: \n";
+			specialArray.print();
+		}
+	}
+	// Add catch statement below
+	catch (runtime_error& except) {
+		//prints the error message passed by throw statement
+		cout << except.what() << endl;
+  }
+	
+	return 0;
+}
