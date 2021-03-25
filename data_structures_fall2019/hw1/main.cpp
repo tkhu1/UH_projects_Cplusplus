426541cd442e579a00a17b691efc58abef95398e
Tyler Hu <46427408+tkhu1@users.noreply.github.com>
2021-03-24 20:08:14 -0500

    removed garbage files

--- data_structures_fall2019/hw1/main.cpp
+++ data_structures_fall2019/hw1/main.cpp
@@ -8,6 +8,10 @@ using namespace std;
 //FUNCTIONS////////////////////////////////////////////////////////////////////////////
 //returns number of ripe kiwis in one row
 int GetRipeNum (int *row, int size) {
+	//defaults to a size of 1 for invalid size argument
+	if (size < 1) {
+		size = 1;
+	}
 	int *p = row;
 	int counter = 0;
 	for (int i = 0; i < size; i++) {
@@ -19,6 +23,10 @@ int GetRipeNum (int *row, int size) {
 }
 //returns number of rotten kiwis in one row
 int GetRottenNum (int *row, int size) {
+	//defaults to a size of 1 for invalid size argument
+	if (size < 1) {
+		size = 1;
+	}
 	int *p = row;
 	int counter = 0;
 	for (int i = 0; i < size; i++) {
@@ -30,6 +38,10 @@ int GetRottenNum (int *row, int size) {
 }
 //checks if row can be harvested
 bool CheckHarvest(int *row, int size) {
+	//defaults to a size of 1 for invalid size argument
+	if (size < 1) {
+		size = 1;
+	}
 	int *p = row;
 	int counter = 0;
 	for (int i = 0; i < size; i++) {
@@ -44,10 +56,10 @@ bool CheckHarvest(int *row, int size) {
 }
 //MAIN/////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
-	/*if (argc != 3 && argc != 2) {
+	if (argc != 3 && argc != 2) {
 		cout << "Invalid arguments" << endl;
 		return 1;
-	}*/
+	}
 
 	ArgumentManager am(argc, argv);
 	const string input = am.get("input");
@@ -55,35 +67,93 @@ int main(int argc, char* argv[]) {
 
 	//init input stream
 	ifstream infs(input);
-
+	//exits if input not found
+	if (!infs) {
+    cout << "Could not open the input file. Exiting program." << endl;
+    return 1;
+  }
+	//error catcher
+	const string errorMsg;
+	//global vars
 	int size, temp;
 	int day = 0;
 	int totalRipe = 0;
 	int totalRotten = 0;
 
-	//gets size of farm
-	infs >> size;
-
-	//init pointer matrix to represent farm
-	int **farm;
-	farm = new int*[size];
-  //creates one farm row at a time
-  for (int i = 0; i < size; i++) {
-		farm[i] = new int[size];
-		//pointer to kiwi
-		int *p = farm[i];
-		for (int j = 0; j < size; j++) {
-			//holds input value
-			infs >> temp;
-			//sets the value for current pointer to kiwi
-			*p = temp;
-			p++;
+	try {
+		//gets size of farm
+		infs >> size;
+		//throws error if farm size is less than 1
+		if (size < 1) {
+			throw (errorMsg);
 		}
 	}
-	infs.close();
+	//catches error and exits
+	catch (const string error) {
+		cout << "Invalid size found for kiwi farm. Exiting program." << endl;
+		return 1;
+	}
 
-	//MAIN 7 DAY LOOP////////////////////////////////////////////////////////////////////
-	do {
+	try {
+		//init pointer matrix to represent farm
+		int **farm;
+		farm = new int*[size];
+		//creates one farm row at a time
+		for (int i = 0; i < size; i++) {
+			farm[i] = new int[size];
+			//pointer to kiwi
+			int *p = farm[i];
+			for (int j = 0; j < size; j++) {
+				//holds input value
+				infs >> temp;
+				//throws error if input is less than 0
+				if (temp < 0) {
+    			throw "Invalid kiwi input value found (must be at least 0). Exiting program.";
+				}
+				//sets the value for current pointer to kiwi
+				*p = temp;
+				p++;
+			}
+		}
+
+		infs.close();
+
+		//MAIN 7 DAY LOOP////////////////////////////////////////////////////////////////////
+		do {
+			for (int i = 0; i < size; i++) {
+				int *row = farm[i];
+				//replants if needed
+				if (CheckHarvest(row, size) == true) {
+					totalRipe += GetRipeNum(row, size);
+					totalRotten += GetRottenNum(row, size);
+					for (int j = 0; j < size; j++) {
+						*row = 0;
+						row++;
+					}
+				}
+				//grows rows
+				else {
+					for (int k = 0; k < size; k++) {
+						*row += 1;
+						row++;
+					}
+				}
+			}
+			day++;
+			/*
+			//DEBUG
+			cout << "Day: " << day << endl << endl;
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					cout << farm[i][j] << " ";
+				}
+			cout << endl;
+			}
+			cout << endl;
+			*/
+		} while (day < 7);
+
+		//checks and harvests for the last time at the end of day 7
 		for (int i = 0; i < size; i++) {
 			int *row = farm[i];
 			//replants if needed
@@ -95,59 +165,34 @@ int main(int argc, char* argv[]) {
 					row++;
 				}
 			}
-			//grows rows
-			else {
-				for (int k = 0; k < size; k++) {
-					*row += 1;
-					row++;
-				}
-			}
 		}
-		day++;
 
-		/*
-		//DEBUG
-		cout << "Day: " << day << endl << endl;
+		//init output stream
+		ofstream outfs(output);
+
+		//outputs to file
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				cout << farm[i][j] << " ";
+				outfs << farm[i][j] << " ";
 			}
-		cout << endl;
+			outfs << endl;
 		}
-		cout << endl;
-		*/
+		outfs << endl << "Harvested kiwis: " << totalRipe << endl;
+		outfs << "Rotten kiwis: " << totalRotten << endl;
 
-	} while (day < 7);
-
-	//checks and harvests for the last time at the end of day 7
-	for (int i = 0; i < size; i++) {
-		int *row = farm[i];
-		//replants if needed
-		if (CheckHarvest(row, size) == true) {
-			totalRipe += GetRipeNum(row, size);
-			totalRotten += GetRottenNum(row, size);
-			for (int j = 0; j < size; j++) {
-				*row = 0;
-				row++;
-			}
-		}
+		outfs.close();
+		delete farm;
 	}
-
-	//init output stream
-	ofstream outfs(output);
-
-	//outputs to file
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			outfs << farm[i][j] << " ";
-		}
-		outfs << endl;
+	//catches error and exits
+	catch (const char* error) {
+		cout << error << endl;
+		return 1;
 	}
-	outfs << endl << "Harvested kiwis: " << totalRipe << endl;
-	outfs << "Rotten kiwis: " << totalRotten << endl;
-
-	outfs.close();
-	delete farm;
+	//general error handling
+	catch (...) { 
+    cout << "Default exception generated. Exiting program." << endl;
+		return 1;
+  } 
 
 	return 0;
 }
